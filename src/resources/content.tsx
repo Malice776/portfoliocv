b4f448502ef6eeba1cffb8d09e85e98842367936
Malice776 <marinerapin@me.com>
2025-11-25 14:43:47 +0100

    A propos finit

--- src/resources/content.tsx
+++ src/resources/content.tsx
@@ -5,7 +5,7 @@ const person: Person = {
   firstName: "Marine",
   lastName: "Rapin",
   name: `Marine Rapin`,
-  role: "Data Engineer",
+  role: "Alternante Data Engineer",
   avatar: "/images/avatar.jpg",
   email: "example@gmail.com",
   location: "Europe/Paris", // Expecting the IANA time zone identifier, e.g., 'Europe/Vienna'
@@ -24,23 +24,23 @@ const social: Social = [
   {
     name: "GitHub",
     icon: "github",
-    link: "https://github.com/once-ui-system",
+    link: "https://github.com/Malice776",
   },
   {
     name: "LinkedIn",
     icon: "linkedin",
-    link: "https://www.linkedin.com/company/once-ui/",
+    link: "https://www.linkedin.com/in/marine-rapin-105095150/",
   },
-  {
-    name: "Threads",
-    icon: "threads",
-    link: "https://www.threads.com/@once_ui",
-  },
-  {
-    name: "Email",
-    icon: "email",
-    link: `mailto:${person.email}`,
-  },
+  // {
+  //   name: "Threads",
+  //   icon: "threads",
+  //   link: "https://www.threads.com/@once_ui",
+  // },
+  // {
+  //   name: "Email",
+  //   icon: "email",
+  //   link: `mailto:${person.email}`,
+  // },
 ];
 
 const home: Home = {
@@ -54,31 +54,31 @@ const home: Home = {
     display: true,
     title: (
       <Row gap="12" vertical="center">
-        
         <Line background="brand-alpha-strong" vert height="20" />
         <Text marginRight="4" onBackground="brand-medium">
-          Featured work
+          Dernier article
         </Text>
+        <Line background="brand-alpha-strong" vert height="20" />
       </Row>
     ),
     href: "/work/building-once-ui-a-customizable-design-system",
   },
   subline: (
     <>
-      Voici mon portfolio de projets scolaire et personnel 
-      <Logo
+      Bienvenue dans mon portfolio : un espace où se rencontrent mes projets scolaires et mes explorations personnelles.  
+      {/* <Logo
         dark
         icon="/trademarks/wordmark-dark.svg"
         style={{ display: "inline-flex", top: "0.25em", marginLeft: "-0.25em" }}
-      />
+      /> */}
     </>
   ),
 };
 
 const about: About = {
   path: "/about",
-  label: "About",
-  title: `About – ${person.name}`,
+  label: "A propos",
+  title: `A propos – ${person.name}`,
   description: `Meet ${person.name}, ${person.role} from ${person.location}`,
   tableOfContent: {
     display: true,
@@ -88,65 +88,91 @@ const about: About = {
     display: true,
   },
   calendar: {
-    display: true,
+    display: false,
     link: "https://cal.com",
   },
   intro: {
     display: true,
     title: "Introduction",
-    description: (
-      <>
-        durant mes deux an de mastère j'ai decouvert l'etandu des possibilité que la programmation offre.
-        J'ai pu apprendre differente langue de programmation et framework tel que React, Next.js, Python et bien d'autre.
-        J'ai aussi pu decouvrir le monde de la data et de l'ia avec des outils comme Tensorflow ou encore
-        des modeles tels que les LLM et des reseaux de neurone.
-      </>
-    ),
+  description: (
+    <>
+      Je suis passionné par l'IA et la data.<br />
+      J'adore analyser, comprendre et exploiter les données. Mon objectif est de donner du sens aux données et de développer des Site qui font vraiment la différence.
+    </>
+  ),
+
   },
   work: {
-    display: false, // set to false to hide this section
-    title: "Work Experience",
+    display: true, // set to false to hide this section
+    title: "Experience professionelle",
     experiences: [
       {
-        company: "FLY",
-        timeframe: "2022 - Present",
-        role: "Senior Design Engineer",
+        company: "AXA IM part of BNP",
+        timeframe: "2024 - Present",
+        role: "Alternante Data Engineer",
         achievements: [
           <>
-            Redesigned the UI/UX for the FLY platform, resulting in a 20% increase in user
-            engagement and 30% faster load times.
+            je travaille sur la gestion et la migration des bases de données vers de nouveaux systèmes, en assurant la qualité et la sécurité des données tout au long du processus.
           </>,
           <>
-            Spearheaded the integration of AI tools into design workflows, enabling designers to
-            iterate 50% faster.
+            Je développe et automatise des scripts pour Windows en utilisant des language tels que PowerShell, STL, Batch et AWK, ce qui permet d’optimiser les flux de travail et d’améliorer l’efficacité des opérations de données.
           </>,
         ],
         images: [
-          // optional: leave the array empty if you don't want to display images
-          {
-            src: "/images/projects/project-01/cover-01.jpg",
-            alt: "Once UI Project",
-            width: 16,
-            height: 9,
-          },
+          // // optional: leave the array empty if you don't want to display images
+          // {
+          //   src: "/images/projects/project-01/cover-01.jpg",
+          //   alt: "Once UI Project",
+          //   width: 16,
+          //   height: 9,
+          // },
         ],
       },
       {
-        company: "Creativ3",
-        timeframe: "2018 - 2022",
-        role: "Lead Designer",
+        company: "Mega International",
+        timeframe: "juin - aout 2023",
+        role: "Stagiaire Solutions Engineer",
         achievements: [
           <>
-            Developed a design system that unified the brand across multiple platforms, improving
-            design consistency by 40%.
+            Participation aux projets techniques et fonctionnels, incluant le développement de sites intranet et la gestion des données sur la plateforme Mega Hopex.
           </>,
           <>
-            Led a cross-functional team to launch a new product line, contributing to a 15% increase
-            in overall company revenue.
+            Réalisation également de traductions de documentation technique du français vers l’anglais et inversement.
           </>,
         ],
         images: [],
       },
+
+      {
+        company: "Centre de recherceh saint-Antoine",
+        timeframe: "Octobre 2023",
+        role: "Stagiaire Médiatrice scientifique",
+        achievements: [
+          <>
+            Participation à la Fête de la Science en contribuant à la création et à l’animation d’un stand de médiation scientifique sur le thème de la santé.
+          </>,
+          <>
+            Sensibilisation du grand public aux avancées scientifiques et technologiques dans le domaine de la santé à travers des activités interactives et éducatives.
+          </>,
+          <>
+            Accompagnement d’une classe de ST2S pour visite de découverte du centre de recherche.
+          </>,
+        ],
+        images: [],
+      },
+
+      // {
+      //   company: "Leclerc",
+      //   timeframe: "été 2022 & été 2024",
+      //   role: "Hote de caisse",
+      //   achievements: [
+      //     <>
+      //        Gestion d'une caisse, participation au rayonnage et hote de caisse automatique.
+      //     </>,
+
+      //   ],
+      //   images: [],
+      // },
     ],
   },
   studies: {
@@ -161,43 +187,55 @@ const about: About = {
         name: "Sorbonne sciences",
         description: <>Licence en informatique, mineur communication et médiation scientifique.</>,
       },
+      {
+        name: "Lycée Martin Luther King",
+        description: <>Baccalalauréat (STI2D) Sciences technologies et de l'industrie du développement durable Mention Bien, major de classe.</>,
+      },
     ],
   },
   technical: {
-    display: false, // set to false to hide this section
-    title: "Technical skills",
+    display: true, // set to false to hide this section
+    title: "Compétences Techniques",
     skills: [
       {
-        title: "Figma",
+        title: "Gestion de bases de données : SQL, NoSQL, MongoDB, PostgreSQL",
         description: (
-          <>Able to prototype in Figma with Once UI with unnatural speed.</>
+          <>Conception, gestion et optimisation de bases de données relationnelles et NoSQL, notamment MongoDB et PostgreSQL, pour assurer la qualité, la performance et la sécurité des données.</>
         ),
         tags: [
           {
-            name: "Figma",
-            icon: "figma",
+            name: "STL",
+            icon: "STL",
+          },
+          {
+            name: "MongoDB",
+            icon: "MongoDB",
+          },
+          {
+            name: "PostgreSQL",
+            icon: "PostgreSQL",
           },
         ],
         // optional: leave the array empty if you don't want to display images
         images: [
-          {
-            src: "/images/projects/project-01/cover-02.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
-          {
-            src: "/images/projects/project-01/cover-03.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
+          // {
+          //   src: "/images/projects/project-01/cover-02.jpg",
+          //   alt: "Project image",
+          //   width: 16,
+          //   height: 9,
+          // },
+          // {
+          //   src: "/images/projects/project-01/cover-03.jpg",
+          //   alt: "Project image",
+          //   width: 16,
+          //   height: 9,
+          // },
         ],
       },
       {
-        title: "Next.js",
+        title: "Création de site WEBs",
         description: (
-          <>Building next gen apps with Next.js + Once UI + Supabase.</>
+          <>Développement de sites web modernes et interactifs en utilisant HTML5, CSS3, JavaScript et des frameworks tels que React, Next.js, Django</>
         ),
         tags: [
           {
@@ -209,18 +247,41 @@ const about: About = {
             icon: "nextjs",
           },
           {
-            name: "Supabase",
-            icon: "supabase",
+            name: "HTML.js",
+            icon: "HTML",
+          },
+          {
+            name: "CSS.js",
+            icon: "CSS",
+          },
+          {
+            name: "Django",
+            icon: "Django",
+          },
+          {
+            name: "React",
+            icon: "React",
           },
         ],
         // optional: leave the array empty if you don't want to display images
         images: [
-          {
-            src: "/images/projects/project-01/cover-04.jpg",
-            alt: "Project image",
-            width: 16,
-            height: 9,
-          },
+
+        ],
+      },  
+      {
+        title: "Intelligence Artificielle et Machine Learning",
+        description: (
+          <>Conception et déploiement de modèles d’intelligence artificielle et de machine learning, incluant les réseaux de neurones et les modèles de type LLM, pour analyser et exploiter les données de manière avancée.</>
+        ),
+        tags: [
+              { name: "IA", icon: "ai" },
+              { name: "Machine Learning", icon: "machine-learning" },
+              { name: "Deep Learning", icon: "deep-learning" },
+              { name: "LLM", icon: "llm" },
+              { name: "TensorFlow", icon: "tensorflow" },
+        ],
+        // optional: leave the array empty if you don't want to display images
+        images: [
         ],
       },  
     ],
